51ec47cedc9be3309f30011a8095e5032af0d71e
Bhuvan <bhavaneshwar.patil936.com>
2024-02-29 18:11:11 +0530

    Initial commit

---
+++ src/main/java/com/srs/svmj/FirstSpring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.srs.svmj.FirstSpring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello")
+    public String hello(@RequestBody String name){
+        return "Hello " + name+"!";
+    }
+}
